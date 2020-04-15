5dcc310c8f49e55c9b192d575b2068413be74693
Corbin <her0me@outlook.com>
2020-04-15 19:35:34 -0400

    Initial Commit

---
+++ master.js
@@ -0,0 +1,8 @@
+
+function twitterClick() {
+  window.location.href = 'https://twitter.com/MichDiv';
+}
+
+function facebookClick() {
+  window.location.href = 'https://www.facebook.com/MichDivFencing';
+}
